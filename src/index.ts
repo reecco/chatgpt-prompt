6f48f0ed45eb92136c9a4f444816754a6e3a6b92
reecco <fredcardoso01@hotmail.com>
2023-03-24 17:36:59 -0300

    feat: add loop run in terminal

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { configCompletion as config } from "./config";
 function main(): void {
   const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-  rl.question(`\n${chalk.blue.bold('Enter: ')}`, async (text) => {
+  rl.question(`\n${chalk.blue.bold('Enter: ')}`, async (text: string) => {
     if (text.toLowerCase() === 'quit' || text.toLowerCase() === 'exit') {
       console.log(chalk.blue.bold('\nBye\n'));
       rl.close();
@@ -18,8 +18,23 @@ function main(): void {
     console.log(chalk.magenta.bold('\nProcessing...\n'));
     await output(text).then(res => console.log(res)).catch(error => console.log(error));
 
-    rl.close();
-    main();
+    rl.question(`\n${chalk.blue.bold('Restart? [Y/n]: ')}`, (answer: string) => {
+      rl.close();
+
+      if (answer.toLowerCase() === 'n') {
+        return;
+      }
+
+      if (answer.toLowerCase() === 'y') {
+        const { stdout } = process;
+        stdout.write("\x1B[2J");
+        stdout.write("\x1B[0f");
+        main();
+        return;
+      }
+
+      console.log(chalk.red.bold('Invalid option.'));
+    });
   })
 }
 
@@ -33,13 +48,13 @@ async function output(text: string): Promise<string> {
     if (error) {
       mkdir(`output/${config.file_extension}`, (error) => {
         if (error) {
-          throw `${chalk.red.bold('An error occurred while creating the folder: ')} ${error}`;
+          throw `${chalk.red.bold('An error occurred while creating the folder: ')} \n${error}`;
         }
       });
     }
     writeFile(pathWrite, data, (error) => {
       if (error)
-        throw `${chalk.red.bold('An error occurred while writing the file: ')} ${error}`;
+        throw `${chalk.red.bold('An error occurred while writing the file: ')} \n${error}`;
     });
   });
 
