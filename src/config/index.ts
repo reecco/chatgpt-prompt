8e1f9b281717dd6aa290899770c1189698655ebc
reecco <fredcardoso01@hotmail.com>
2023-03-24 17:37:15 -0300

    feat: add handling errors when reading the configuration file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import { Configuration, OpenAIApi } from 'openai';
 import { config } from 'dotenv';
 import { readFileSync } from 'fs';
+import chalk from 'chalk';
 
 config();
 
-export const configCompletion = JSON.parse(readFileSync('src/config/static/config.json', { encoding: 'utf-8' }));
+const configJsonFile = () => {
+  try {
+    return JSON.parse(readFileSync('src/config/static/config.json', { encoding: 'utf-8' }));
+  } catch (error) {
+    throw `${chalk.red.bold('An error occurred while reading configuration file (config.json): ')} \n${error}`;
+  }
+}
+
+export const configCompletion = configJsonFile();
 
 export const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_KEY }));
